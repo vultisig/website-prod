8ece64ae4dd79f9c6ad6e0b506999a2ba6198a65
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-08-26 17:42:06 +0330

    Fix gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Navbar from "@/components/navbar"
+import GoogleAnalytics from "@/components/ga"
+import Script from "next/script"
 import Footer from "@/components/footer"
 
 export const metadata: Metadata = {
@@ -205,6 +207,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body style={{ background: 'var(--background)' }}>
+        <GoogleAnalytics />
+        {/* Twitter conversion script */}
+        <Script id="twitter-uwt" strategy="afterInteractive">
+          {`
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','ooes4');
+          `}
+        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
