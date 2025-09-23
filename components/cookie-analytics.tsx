2f57a7b141934ec44f2e4186c9f82973633591f8
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-09-23 17:37:48 +0330

    Refactor CookieAnalytics component to load analytics script dynamically. Updated script attributes and removed redundant code for improved performance.

--- components/cookie-analytics.tsx
+++ components/cookie-analytics.tsx
@@ -1,25 +1,25 @@
 "use client";
-
 import Script from "next/script";
 
 export default function CookieAnalytics() {
   return (
     <Script
-      id="cookie-analytics"
-      src="https://cdn.markfi.xyz/scripts/analytics/0.11.24/cookie3.analytics.min.js"
+      id="cookie-analytics-loader"
       strategy="afterInteractive"
-      crossOrigin="anonymous"
-      integrity="sha384-ihnQ09PGDbDPthGB3QoQ2Heg2RwQIDyWkHkqxMzq91RPeP8OmydAZbQLgAakAOfI"
-      onLoad={() => {
-        const script = document.querySelector(
-          "#cookie-analytics"
-        ) as HTMLScriptElement;
-        if (script) {
-          script.setAttribute(
-            "site-id",
-            "44beb5bb-3d65-4e6a-9631-3d99382ca2ea"
-          );
-        }
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function () {
+            if (document.querySelector('script[data-cookie3="1"]')) return;
+            var s = document.createElement('script');
+            s.defer = true;
+            s.crossOrigin = 'anonymous';
+            s.src = 'https://cdn.markfi.xyz/scripts/analytics/0.11.24/cookie3.analytics.min.js';
+            s.setAttribute('site-id','44beb5bb-3d65-4e6a-9631-3d99382ca2ea');
+            s.integrity = 'sha384-ihnQ09PGDbDPthGB3QoQ2Heg2RwQIDyWkHkqxMzq91RPeP8OmydAZbQLgAakAOfI';
+            s.setAttribute('data-cookie3','1');
+            document.head.appendChild(s);
+          })();
+        `,
       }}
     />
   );
