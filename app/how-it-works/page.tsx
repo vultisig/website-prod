a6d68f798a76045fc3bd2e428fc345a7d21dd13e
paaao <167348323+realpaaao@users.noreply.github.com>
2026-07-28 11:14:58 +0200

    v5(how-it-works): three steps

--- app/how-it-works/page.tsx
+++ app/how-it-works/page.tsx
@@ -2,12 +2,14 @@ import dynamic from "next/dynamic"
 
 import Hero from "./components/hero"
 
+const ThreeSteps = dynamic(() => import("./components/three-steps"))
 const FooterBanner = dynamic(() => import("@/components/footer-banner"))
 
 export default function HowItWorks() {
   return (
     <main className="min-h-screen bg-v5-page">
       <Hero />
+      <ThreeSteps />
       <FooterBanner />
     </main>
   )
